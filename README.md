2db7c8d74b8a7c05b4367e4996460100eb93f54c
railsjack <wipying@gmail.com>
2019-07-20 14:05:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Refer to run electron project
+
+```sh
+$ yarn
+$ yarn start
+$ yarn package:win
+```
---
+++ player/custom.js
@@ -0,0 +1,99 @@
+document.ready = function (callback) {
+    if (typeof callback === 'function') {
+        var onceCalled = false;
+        document.addEventListener("DOMContentLoaded", function (event) {
+            if (!onceCalled) {
+                onceCalled = true;
+                callback(event);
+            }
+        });
+        document.onreadystatechange = function (event) {
+            if (document.readyState == "interactive") {
+                if (!onceCalled) {
+                    onceCalled = true;
+                    callback(event);
+                }
+            }
+        }
+    }
+};
+
+var Player = function () {
+    var _play = function (index) {
+        video_player.src = 'D:/torrent-downloads/react-native-26G/ReactNativeCourse(2nd)/videos/files/' + mp4_files[index]
+        playerCaption.src = 'D:/torrent-downloads/react-native-26G/ReactNativeCourse(2nd)/videos/files/' + vtt_files[index]
+        video_player.textTracks[0].mode = 'showing';
+
+        video_player.play();
+    }
+    return {
+        play: _play
+    }
+}();
+
+var TutorialList = function () {
+    var _validateTitle = function (title) {
+        var ret = title;
+        ret = ret.replace(/\.vtt/gi, '');
+        return ret.replace(/\-/gi, ' ');
+    };
+
+    var _load = function(){
+        
+    };
+
+    var _load_list = function(){
+        const dialog = require('electron').remote.dialog
+        const files = dialog.showOpenDialog(null, { 
+            properties: [ 'openDirectory' ],
+            defaultPath: '.'
+        })
+        console.log(files);
+    };
+
+    var _load_html = function () {
+    
+        var temp_vtt_file_dir = '';
+        var optionNode;
+
+        optionNode = document.createElement('option')
+        optionNode.value = ''
+        optionNode.innerHTML = "Select a video to play"
+        tutorialList.appendChild(optionNode)
+
+        for (var k in vtt_files) {
+            var vtt_file = vtt_files[k]
+            var vtt_file_dir = vtt_file.split(/\//gi)[0]
+
+            if (vtt_file_dir != temp_vtt_file_dir) {
+                optionNode = document.createElement('optgroup')
+                optionNode.label = _validateTitle(vtt_file_dir)
+                tutorialList.appendChild(optionNode)
+            }
+
+            optionNode = document.createElement('option')
+            optionNode.value = k
+            optionNode.innerHTML = _validateTitle(vtt_file.replace(vtt_file_dir + "/", '&nbsp;&nbsp;&nbsp;'))
+            tutorialList.appendChild(optionNode)
+            temp_vtt_file_dir = vtt_file_dir
+        }
+
+        tutorialList.appendChild(optionNode)
+    }
+    return {
+        load: _load
+    }
+}();
+
+
+
+document.ready(function (event) {
+    TutorialList.load()
+    tutorialList.addEventListener('change', function (e) {
+        Player.play(e.target.value)
+    });
+
+});
+
+
+
