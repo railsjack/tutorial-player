6c147ae57d9d8fbf97ab5346c5a70dcfd1c40386
railsjack <wipying@gmail.com>
2019-08-27 18:49:06 -0300

    created a class for seekbar, it's not impletmented yet

--- player/custom.js
+++ player/custom.js
@@ -194,12 +194,52 @@ class TutorialList {
     }
 }
 
+class Seekbar {
+
+    constructor() {
+        this.UI = document.getElementById("seekbar");
+        this.posUI = document.getElementById("seekbar_pos");
+        this.UI.ontimeupdate = function () {
+            var percentage = (vid.currentTime / vid.duration) * 100;
+            $("#custom-seekbar span").css("width", percentage + "%");
+        };
+
+        this.drag_started = false;
+
+        this.posUI.onmousedown = (e) => {
+            console.log('onmousedown...');
+            this.drag_started = true;
+        };
+        this.UI.onmousemove = (e) => {
+            if (this.drag_started) {
+                console.log('onmousemove...');
+                this.posUI.style.left = e.pageX;
+            }
+        };
+        this.UI.onmouseup = (e) => {
+            console.log('onmouseup...');
+            this.drag_started = false;
+        };
+        this.posUI.onmouseup = (e) => {
+            console.log('onmouseup...');
+            this.drag_started = false;
+        };
+    }
+
+
+    bindEvents = (evName, callback) => {
+        this.UI.addEventListener(evName, callback)
+        this.UI[evName] = callback
+    }
+
+};
 
 
 document.ready(async (event) => {
 
     const tutorialList = new TutorialList()
     const player = new Player()
+
     await tutorialList.load()
 
     tutorialList.bindEvents('change', (e) => {
